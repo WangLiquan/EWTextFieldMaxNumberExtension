9710924b5be5f8a963474953dfa1c86c052c21a6
wangliquan <liquan.wang@croninfo.com>
2019-03-14 11:17:33 +0800

    集成swiftlint 进行代码格式优化

--- EWTextFieldMaxNumberExtension/UITextField+Extension.swift
+++ EWTextFieldMaxNumberExtension/UITextField+Extension.swift
@@ -11,36 +11,36 @@ import UIKit
 /// 默认最大输入字数为15
 var maxTextNumberDefault = 15
 
-extension UITextField{
+extension UITextField {
     /// 以runtime的形式UITextField添加最大输入字数属性
     public var maxTextNumber: Int {
         set {
             objc_setAssociatedObject(self, &maxTextNumberDefault, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY_NONATOMIC)
         }
         get {
-            if let rs = objc_getAssociatedObject(self, &maxTextNumberDefault) as? Int {
-                return rs
+            if let max = objc_getAssociatedObject(self, &maxTextNumberDefault) as? Int {
+                return max
             }
             return 15
         }
     }
     /// 添加限制最大输入字数target
-    public func addChangeTextTarget(){
+    public func addChangeTextTarget() {
         self.addTarget(self, action: #selector(changeText), for: .editingChanged)
     }
-    @objc private func changeText(){
+    @objc private func changeText() {
         //判断是不是在拼音状态,拼音状态不截取文本
-        if let positionRange = self.markedTextRange{
+        if let positionRange = self.markedTextRange {
             guard self.position(from: positionRange.start, offset: 0) != nil else {
                 checkTextFieldText()
                 return
             }
-        }else {
+        } else {
             checkTextFieldText()
         }
     }
     /// 检测如果输入数高于设置最大输入数则截取
-    private func checkTextFieldText(){
+    private func checkTextFieldText() {
         guard (self.text?.utf16.count)! <= maxTextNumber  else {
             guard let text = self.text else {
                 return
@@ -53,4 +53,3 @@ extension UITextField{
         }
     }
 }
-
